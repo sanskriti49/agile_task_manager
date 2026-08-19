11c21382a493ebb2dfc88b7227b8fbe4a24cc2a9
sanskriti49 <sanskriti0409@gmail.com>
2026-08-19 21:36:33 +0530

    fix(cors): allow wildcard origins for socket.io and express

--- server/server.js
+++ server/server.js
@@ -21,13 +21,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
 	cors: {
-		origin: [
-			"http://localhost:5173",
-			"http://localhost:3000",
-			"http://127.0.0.1:5173",
-		],
+		origin: "*",
 		methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-		credentials: true,
 	},
 });
 
